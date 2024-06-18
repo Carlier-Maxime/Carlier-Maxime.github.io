eb53c141556a7854a3ee5d06bffb0fe8d18d6986
maxime_carlier <28m06c02@gmail.com>
2024-06-18 10:44:47 +0200

    update about

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
-const languages = ['C','C++','Java','Python','PHP','HTML','CSS','Typescript','SQL','Bash'];
+const languages = ['C','C++','Java','Python','PHP','HTML','CSS','Typescript','SQL','Bash','JavaScript'];
 const logiciels = ['CLion','Intellij IDEA','PhpStorm','PyCharm','WebStorm','Visual studio code','Visual studio','Notepad++','Unreal Engine','Git'];
-const frameworks = ['SDL','Laravel','Angular','Qt','Pyqt','Java Swing','Bootstrap','Tensorflow','Keras'];
+const frameworks = ['SDL','Laravel','Angular','Qt','Pyqt','Java Swing','Bootstrap','Tensorflow','Keras','PyTorch','OpenGL','GLFW','Vulkan'];
 const se = ['Linux','Ubuntu','Fedora','Windows 11','Windows 10','Windows 7'];
 
 const len = Math.max(languages.length,logiciels.length,frameworks.length,se.length);
@@ -9,10 +9,10 @@ let tab = document.getElementById("tabCompBody");
 
 for (let i=0; i<len; i++) {
     let line = "<tr>";
-    if (languages[i]) line += "<td>"+languages[i]+"</td>";
-    if (logiciels[i]) line += "<td>"+logiciels[i]+"</td>";
-    if (frameworks[i]) line += "<td>"+frameworks[i]+"</td>";
-    if (se[i]) line += "<td>"+se[i]+"</td>";
+    line += "<td>"+(languages[i] ? languages[i] : "")+"</td>";
+    line += "<td>"+(logiciels[i] ? logiciels[i] : "")+"</td>";
+    line += "<td>"+(frameworks[i] ? frameworks[i] : "")+"</td>";
+    line += "<td>"+(se[i] ? se[i] : "")+"</td>";
     line += "</tr>";
     tab.innerHTML += line;
 }
