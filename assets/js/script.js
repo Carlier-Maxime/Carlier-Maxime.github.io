dd7f8286b8c01ab766cc43ed0cc34dd8df7dc83e
M.EtOuais <28m06c02@gmail.com>
2025-09-04 19:54:13 +0200

    show date

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,13 +203,17 @@ function competenceToHTML(competence) {
     return '<li>'+name+'</li>';
 }
 
+function dateToHTML(name, date) {
+    return '<div class="date"><span class="date-name">'+name+'</span><br><span class="date">'+date+'</span></div>';
+}
+
 function addProject(project) {
     let container = document.getElementById("container");
     let add = '<div id="project"><div class="project-details">'+
     '<h1>'+project['name']+'</h1>'+'<p class="description">'+project['description']+'</p>'+
-    '<div class="control"><button class="btn"><a href="'+project['link']+'"><span class="code">Code</span>'+
+    '<div class="control">'+dateToHTML('Date de début', project['startDate'])+'<button class="btn"><a href="'+project['link']+'"><span class="code">Code</span>'+
     '<span class="logo-github"><i class="fa-brands fa-github"></i></span><span class="github">Github</span>'+
-    '</a></button></div></div><div class="project-img"><img src="./assets/images/'+project['img']+'" alt="apercu\">'+
+    '</a></button>'+dateToHTML('Dernière MAJ', project['lastUpdate'])+'</div></div><div class="project-img"><img src="./assets/images/'+project['img']+'" alt="apercu\">'+
     '<div class="info"><h2>Compétences</h2><ul>';
     project['competences'].forEach(competence => {
         add+=competenceToHTML(competence);
