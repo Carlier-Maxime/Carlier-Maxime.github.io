7ba140362df9ae8521967f3e6cb065dbc8b394f3
M_EtOuais <28m06c02@gmail.com>
2026-01-06 22:55:24 +0100

    :lipstick: :recycle: Refactor project layout and media handling in CSS and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ import { projects } from "./project.js";
 function competenceToHTML(competence) {
     let name = competence.name;
     let link = competence.link;
-    if (link) return '<li><a href="'+link+'">'+name+'</a></li>';
-    return '<li>'+name+'</li>';
+    return `<a ${link ? `href="${link}"` : ''}> ${name} </a>`;
 }
 
 function dateToHTML(name, date) {
@@ -13,16 +12,44 @@ function dateToHTML(name, date) {
 
 function addProject(project) {
     let container = document.getElementById("container");
-    let add = '<div id="project"><div class="project-details">'+
-    '<h1>'+project['name']+'</h1>'+'<p class="description">'+project['description']+'</p>'+
-    '<div class="control">'+dateToHTML('Date de début', project['startDate'])+'<button class="btn"><a href="'+project['link']+'"><span class="code">Code</span>'+
-    '<span class="logo-github"><i class="fa-brands fa-github"></i></span><span class="github">Github</span>'+
-    '</a></button>'+dateToHTML('Dernière MAJ', project['lastUpdate'])+'</div></div><div class="project-img"><img src="./assets/images/'+project['img']+'" alt="apercu\">'+
-    '<div class="info"><h2>Compétences</h2><ul>';
-    project['competences'].forEach(competence => {
-        add+=competenceToHTML(competence);
+
+    let mediaSrc = `./assets/images/${project['img']}`;
+    let mediaHTML;
+    if (project['img'].endsWith('.webm')) {
+        mediaHTML = `<video src="${mediaSrc}" autoplay loop muted playsinline></video>`;
+    } else {
+        mediaHTML = `<img src="${mediaSrc}" alt="aperçu">`;
+    }
+    mediaHTML = `<div class="media">${mediaHTML}</div>`;
+
+    let badgesHTML = '<div class="badges">';
+    project['competences'].forEach(comp => {
+        badgesHTML += competenceToHTML(comp);
     });
-    container.innerHTML += add+'</ul></div></div></div>';
+    badgesHTML += '</div>';
+
+    let add = `
+    <div id="project">
+        <div class="project-details">
+            <h1>${project['name']}</h1>
+            <p class="description">${project['description']}</p>
+            ${badgesHTML}
+            <div class="control">
+                ${dateToHTML('Date de début', project['startDate'])}
+                <button class="btn">
+                    <a href="${project['link']}">
+                        <span class="code">Code</span>
+                        <span class="logo-github"><i class="fa-brands fa-github"></i></span>
+                        <span class="github">Github</span>
+                    </a>
+                </button>
+                ${dateToHTML('Dernière MAJ', project['lastUpdate'])}
+            </div>
+        </div>
+        ${mediaHTML}
+    </div>`;
+
+    container.innerHTML += add;
 }
 
 projects.forEach(project => {
